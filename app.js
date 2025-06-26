cd8fd3762b8d191575803f2c9985118e561abdf3
Nada-Mosab <nadajkg78@gmail.com>
2025-06-26 12:59:08 +0300

    Added quote app and quotes.json file

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Hello from Node.js!");
+const fs = require('fs');
+
+// Read quotes from the JSON file
+const data = fs.readFileSync('./quotes.json', 'utf8');
+const quotes = JSON.parse(data);
+
+// Pick a random quote
+const randomIndex = Math.floor(Math.random() * quotes.length);
+const quote = quotes[randomIndex];
+
+// Show it in the terminal
+console.log("💬 Quote of the Day:");
+console.log(`👉 "${quote}"`);
+
